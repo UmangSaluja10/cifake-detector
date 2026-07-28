4e979b206b1c25abfaa60fa6f2d31b6756466485
UmangSaluja10 <umangsaluja99@gmail.com>
2026-07-28 17:45:53 +0530

    Changes in files to deploy

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ app = FastAPI(title="CIFAKE Detector API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
